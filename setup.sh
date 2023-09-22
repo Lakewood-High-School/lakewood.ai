ef4071ddcd32e4d6c8d239480024fbd4059be637
Luna Kilborn <leland.kilborn@hotmail.com>
2023-09-22 19:04:49 -0400

    Add symlink detection to setup
    Now the setup.sh file can detect if the configuration file symlink
    already exists and if it points to the wrong place

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
-printf "Creating configuration file symlink"
-ln -s `pwd`/lakewood.ai.conf /etc/apache2/sites-available/lakewood.ai.conf
-if [ $? -eq 0 ]; then
-	echo " [DONE]"
+APACHE_CONFIG_PATH=/etc/apache2-test/sites-available/lakewood.ai.conf
+
+function CreateConfigurationFile() {
+	printf "Creating configuration file symlink"
+	ln -s `pwd`/lakewood.ai.conf $APACHE_CONFIG_PATH
+	if [ $? -eq 0 ]; then
+		echo " [DONE]"
+	else
+		echo " [ERROR]"
+	fi
+}
+
+if [ -f $APACHE_CONFIG_PATH ]; then
+	if [ `realpath $APACHE_CONFIG_PATH` != `pwd`/lakewood.ai.conf ]; then
+		echo "Symlink exists, but points to wrong place"
+	else
+		echo "Configuration file symlink already created"
+	fi
 else
-	echo " [ERROR]"
+	CreateConfigurationFile
 fi
 echo "Completed setup"
