0c5380a5165613892d44000687daf100c8c13e40
Luna Kilborn <leland.kilborn@hotmail.com>
2023-09-24 20:17:50 -0400

    Move asset/ out of dist/

--- src/home/typescript/script.ts
+++ src/home/typescript/script.ts
@@ -1,12 +1,12 @@
 // Bootstrap
-import '../../../dist/assets/js/core/bootstrap.bundle.min.js';
+import '../../../kit/js/core/bootstrap.bundle.min.js';
 
 // Charts!
 // We don't need it right now and it quintuples the size of the bundle
 // import '../../../dist/assets/js/plugins/chartjs.min.js';
 
 // Primary material dashboard js file
-import '../../../dist/assets/js/material-dashboard.js';
+import '../../../kit/js/material-dashboard.js';
 
 import { test } from './import_test';
 
