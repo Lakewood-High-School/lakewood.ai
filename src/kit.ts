aca6edfe0822d33d4575cae84fd26e5aa4d78181
Luna Kilborn <leland.kilborn@hotmail.com>
2023-09-28 18:44:29 -0400

    Add forgotten files

---
+++ src/kit.ts
@@ -0,0 +1,10 @@
+// Bootstrap
+import '../kit/js/core/bootstrap.bundle.min.js';
+
+// Charts!
+// We don't need it right now and it quintuples the size of the bundle
+// import '../../../dist/assets/js/plugins/chartjs.min.js';
+
+// Primary material dashboard js file
+import '../kit/js/material-dashboard.js';
+
